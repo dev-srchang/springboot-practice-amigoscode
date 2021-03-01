1a7065ff272fb7347cb8cee29154888cfb8ff78a
dev-srchang <dev.srchang@gmail.com>
2021-03-01 21:04:16 +0900

    student class

--- src/main/java/inc/src/demo/DemoApplication.java
+++ src/main/java/inc/src/demo/DemoApplication.java
@@ -1,10 +1,13 @@
 package inc.src.demo;
 
+import inc.src.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -23,5 +26,10 @@ public class DemoApplication {
 	@GetMapping(path = "/list")
 	public List<String> getHiWorldList() {
 		return List.of("Hi", "World", "List");
+	}
+
+	@GetMapping(path = "/student")
+	public List<Student> getTestStudent() {
+		return List.of(new Student(1L, "srchang", "srchang@gmail.com", LocalDate.of(1975, Month.MAY, 22), 46));
 	}
 }
