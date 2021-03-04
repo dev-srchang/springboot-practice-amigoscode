a16426a113eb4aa91a8f97fbb7b2ccb908d13355
ywm.srchang <srchang@ywmobile.co.kr>
2021-03-05 01:31:45 +0900

    JPA Repository

--- src/main/java/inc/src/demo/student/StudentService.java
+++ src/main/java/inc/src/demo/student/StudentService.java
@@ -1,15 +1,22 @@
 package inc.src.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @Service
 public class StudentService {
 
+    private StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(new Student(1L, "srchang", "srchang@gmail.com", LocalDate.of(1975, Month.MAY, 22), 46));
+        return studentRepository.findAll();
+//        return List.of(new Student(1L, "srchang", "srchang@gmail.com", LocalDate.of(1975, Month.MAY, 22), 46));
     }
 }
