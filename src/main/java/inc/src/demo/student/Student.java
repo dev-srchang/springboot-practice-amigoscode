7013c9fb3ed514e0159557429afe0d2051410c63
ywm.srchang <srchang@ywmobile.co.kr>
2021-03-05 01:13:34 +0900

    JPA & @Entity / JPA in Action

--- src/main/java/inc/src/demo/student/Student.java
+++ src/main/java/inc/src/demo/student/Student.java
@@ -1,8 +1,22 @@
 package inc.src.demo.student;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table
 public class Student {
+
+    @Id
+    @SequenceGenerator(
+        name = "student_sequence",
+        sequenceName = "student_sequence",
+        allocationSize = 1
+    )
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "student_sequence"
+    )
     Long id;
     String name;
     String email;
